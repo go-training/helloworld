05837f549bdaaeff153e17cb5facc7fe092f2757
Alan Ranciato <alanranciato@gmail.com>
2019-08-07 15:40:15 -0400

    added web handler

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
-
-func helloworld() string {
-	return "Hello World!!"
-}
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-	fmt.Println(helloworld())
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+    http.ListenAndServe(":8080", nil)
 }

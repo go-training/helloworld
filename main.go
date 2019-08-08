2aacf9ef6c2dde5a1bafe255d2371d5fcd44157d
Alan Ranciato <alanranciato@gmail.com>
2019-08-08 11:31:50 -0400

    formatting

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+        fmt.Fprintf(w, "<div style: font-size 20px;font-weight:bold;>Hello <p/>Version 1</div>)
     })
 
     http.ListenAndServe(":8080", nil)

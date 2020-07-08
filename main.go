2eb9fa0ded4b2a884dcfd9729e9376d1cc956a2c
Marcelo Melo <marcelo@marcelomelo.com>
2020-07-08 15:49:18 -0300

    make output http available

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func helloworld() string {
-	return "Hello World!!"
+func handler(w http.ResponseWriter, r *http.Request) {
+	var name, _ = os.Hostname()
+
+	fmt.Fprintf(w, "<h1>This request was processed by host: %s</h1>\n", name)
 }
 
 func main() {
-	for {
-		fmt.Println(helloworld())
-	}
-
+	fmt.Fprintf(os.Stdout, "Web Server started. Listening on 0.0.0.0:8080\n")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }

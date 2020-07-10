1494fe319c467c48d5e2c32ed58aa65a17cd766c
Marcelo Melo <marcelo@marcelomelo.com>
2020-07-10 09:03:40 -0300

    testing branch

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	var name, _ = os.Hostname()
 
-	fmt.Fprintf(w, "<h1>v2: -This request was processed by host: %s</h1>\n", name)
+	fmt.Fprintf(w, "<h1>Marcelo Test: This request was processed by pod: %s</h1><h1>fix-test1 branch</h1>\n", name)
 }
 
 func main() {
